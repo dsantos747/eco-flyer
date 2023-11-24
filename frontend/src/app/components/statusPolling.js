9beee4f41a6d4e248692e38fbcc459c780a1c5f9
dsantos747 <d.santos74747@gmail.com>
2023-11-24 21:11:33 +0000

    Handled any errors sprouting from Flask backend

--- frontend/src/app/components/statusPolling.js
+++ frontend/src/app/components/statusPolling.js
@@ -7,42 +7,49 @@ import { emissionsError } from './exceptions';
 export function StatusPoll({ taskID }) {
   const [taskStatus, setTaskStatus] = useState('Not ready');
   const [errorCode, setErrorCode] = useState(0);
+  // const [errorCode, setErrorCode] = useState(false);
   const router = useRouter();
 
-  useEffect(() => {
-    if (errorCode !== 0) {
-      throw new emissionsError(`Error: ${errorCode}`);
-    }
-  }, [errorCode]);
+  // useEffect(() => {
+  //   if (errorCode !== 0) {
+  //     throw new emissionsError(`Error: ${errorCode}`);
+  //   }
+  // }, [errorCode]);
 
   useEffect(() => {
     const pollTaskStatus = async (taskID) => {
-      const intervalId = setInterval(async () => {
-        try {
-          const pollResponse = await checkRedis('response', taskID); // Modify this loop to instead check if a "data"
-          if (pollResponse === 1) {
-            clearInterval(intervalId);
-            setTaskStatus('Task is now ready');
-            router.push(`/results/${taskID}`);
-          }
-          const pollError = await checkRedis('error', taskID);
-          if (pollError === 1) {
-            const error = await getRedis('error', taskID);
-            console.log(`The error is ${error}`);
-            setErrorCode(error);
-            // throw new Error('Need to use content of error_id to determine what to throw here');
-          }
-        } catch (error) {
-          // How do you get the above error to trigger this catch block?
-          console.error('Error polling task status:', error);
-          if (error.message === 'Need to use content of error_id to determine what to throw here') {
-            clearInterval(intervalId);
-          }
-          clearInterval(intervalId);
+      const pollInterval = setInterval(async () => {
+        // try {
+        const pollResponse = await checkRedis('response', taskID);
+        if (pollResponse === 1) {
+          clearInterval(pollInterval);
+          setTaskStatus('Task is now ready');
+          router.push(`/results/${taskID}`);
         }
+        const pollError = await checkRedis('error', taskID);
+        if (pollError === 1 && errorCode === 0) {
+          clearInterval(pollInterval);
+          const error = await getRedis('error', taskID);
+          const errorText = await JSON.parse(error);
+          setErrorCode(error);
+          setErrorCode(() => {
+            throw new Error(errorText);
+          });
+
+          // setErrorCode(error);
+          // throw new Error('Need to use content of error_id to determine what to throw here');
+        }
+        // } catch (error) {
+        //   // How do you get the above error to trigger this catch block?
+        //   console.error('Error polling task status:', error);
+        //   if (error.message === 'Need to use content of error_id to determine what to throw here') {
+        //     clearInterval(pollInterval);
+        //   }
+        //   clearInterval(pollInterval);
+        // }
       }, 2000);
 
-      return () => clearInterval(intervalId);
+      return () => clearInterval(pollInterval);
     };
 
     pollTaskStatus(taskID);
