897ed57326eaaea48d1b44bab838f05ecbb6384d
dsantos747 <d.santos74747@gmail.com>
2024-01-21 17:48:11 +0000

    Refactored VapourTrails animation for performance

--- frontend/src/app/(landing)/layout.js
+++ frontend/src/app/(landing)/layout.js
@@ -4,13 +4,14 @@ import { VapourTrails } from '../components/vapourTrails';
 
 export default function LandingLayout({ children }) {
   return (
-    <section className="min-h-screen flex flex-col justify-between bg-gradient-to-t from-rose-100 to-blue-400 to-70%">
+    <section className='min-h-screen flex flex-col justify-between bg-gradient-to-t from-rose-100 to-blue-400 to-70%'>
       <Header></Header>
       <VapourTrails></VapourTrails>
-      <div className="backdrop-blur-[2px] w-full h-full z-10 flex flex-col flex-1 grow justify-center items-center">
-        <div className="flex flex-1 flex-col place-content-center pt-16 pb-2">{children}</div>
+      <div className='w-full h-full z-10 flex flex-col flex-1 grow justify-center items-center'>
+        {/* <div className='backdrop-blur-[2px] w-full h-full z-10 flex flex-col flex-1 grow justify-center items-center'> */}
+        <div className='flex flex-1 flex-col place-content-center pt-16 pb-2'>{children}</div>
       </div>
-      <div className="relative z-[5]">
+      <div className='relative z-[5]'>
         <MainFooter></MainFooter>
       </div>
     </section>
--- frontend/src/app/components/vapourTrails.js
+++ frontend/src/app/components/vapourTrails.js
@@ -8,27 +8,13 @@ import styles from './planes.module.css';
 
 export function VapourTrails() {
   useEffect(() => {
-    // const planeAnimation = anime.timeline({
-    //   autoplay: true,
-    //   // delay: 200,
-    //   loop: true,
-    // });
-    // planeAnimation.add({
-    //   targets: '.animeTarget',
-    //   translateX: ['100%', '-100%'],
-    //   easing: 'linear',
-    //   duration: 12000,
-    //   delay: anime.stagger(1000),
-    //   autoplay: true,
-    // });
-    const planeAnimation = anime({
+    anime({
       autoplay: true,
       targets: '.animeTarget',
       translateX: ['100%', '-100%'],
       easing: 'linear',
-      duration: 20000,
+      duration: 24000,
       delay: anime.stagger(1000),
-      // loop: true,
       update: function (animation) {
         if (animation.progress > 68) {
           animation.restart();
@@ -39,70 +25,23 @@ export function VapourTrails() {
 
   return (
     <div
-      aria-hidden="true"
-      className="fixed top-0 left-0 flex items-center h-screen w-screen max-w-screen justify-center overflow-hidden select-none"
-    >
-      <div className="h-full flex items-center justify-center overflow-hidden">
-        <div className={styles.planePath}>
-          <div className={`${styles.plane} animeTarget`}>
-            <FontAwesomeIcon icon={faPlane} className="rotate-180 text-2xl text-rose-200" />
-            <object type="image/svg+xml" data="/vapourTrails.svg" width="555" height="15"></object>
+      aria-hidden='true'
+      className='fixed top-0 left-0 flex items-center h-screen w-screen max-w-screen justify-center overflow-hidden select-none'>
+      <div className='h-full flex items-center justify-center overflow-hidden'>
+        {Array.from({ length: 10 }, (_, i) => (
+          <div key={i} className={`${styles.planePath}`}>
+            <div className={`${styles.plane} animeTarget`}>
+              <FontAwesomeIcon icon={faPlane} className='rotate-180 text-2xl text-rose-200 blur-[2px]' />
+              <object
+                type='image/svg+xml'
+                data='/vapourTrails.svg'
+                width='555'
+                height='15'
+                aria-label='Plane animation trail svg'
+                aria-hidden></object>
+            </div>
           </div>
-        </div>
-        <div className={styles.planePath}>
-          <div className={`${styles.plane} animeTarget`}>
-            <FontAwesomeIcon icon={faPlane} className="rotate-180 text-2xl text-rose-200" />
-            <object type="image/svg+xml" data="/vapourTrails.svg" width="555" height="15"></object>
-          </div>
-        </div>
-        <div className={styles.planePath}>
-          <div className={`${styles.plane} animeTarget`}>
-            <FontAwesomeIcon icon={faPlane} className="rotate-180 text-2xl text-rose-200" />
-            <object type="image/svg+xml" data="/vapourTrails.svg" width="555" height="15"></object>
-          </div>
-        </div>
-        <div className={styles.planePath}>
-          <div className={`${styles.plane} animeTarget`}>
-            <FontAwesomeIcon icon={faPlane} className="rotate-180 text-2xl text-rose-200" />
-            <object type="image/svg+xml" data="/vapourTrails.svg" width="555" height="15"></object>
-          </div>
-        </div>
-        <div className={styles.planePath}>
-          <div className={`${styles.plane} animeTarget`}>
-            <FontAwesomeIcon icon={faPlane} className="rotate-180 text-2xl text-rose-200" />
-            <object type="image/svg+xml" data="/vapourTrails.svg" width="555" height="15"></object>
-          </div>
-        </div>
-        <div className={styles.planePath}>
-          <div className={`${styles.plane} animeTarget`}>
-            <FontAwesomeIcon icon={faPlane} className="rotate-180 text-2xl text-rose-200" />
-            <object type="image/svg+xml" data="/vapourTrails.svg" width="555" height="15"></object>
-          </div>
-        </div>
-        <div className={styles.planePath}>
-          <div className={`${styles.plane} animeTarget`}>
-            <FontAwesomeIcon icon={faPlane} className="rotate-180 text-2xl text-rose-200" />
-            <object type="image/svg+xml" data="/vapourTrails.svg" width="555" height="15"></object>
-          </div>
-        </div>
-        <div className={styles.planePath}>
-          <div className={`${styles.plane} animeTarget`}>
-            <FontAwesomeIcon icon={faPlane} className="rotate-180 text-2xl text-rose-200" />
-            <object type="image/svg+xml" data="/vapourTrails.svg" width="555" height="15"></object>
-          </div>
-        </div>
-        <div className={styles.planePath}>
-          <div className={`${styles.plane} animeTarget`}>
-            <FontAwesomeIcon icon={faPlane} className="rotate-180 text-2xl text-rose-200" />
-            <object type="image/svg+xml" data="/vapourTrails.svg" width="555" height="15"></object>
-          </div>
-        </div>
-        <div className={styles.planePath}>
-          <div className={`${styles.plane} animeTarget`}>
-            <FontAwesomeIcon icon={faPlane} className="rotate-180 text-2xl text-rose-200" />
-            <object type="image/svg+xml" data="/vapourTrails.svg" width="555" height="15"></object>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
